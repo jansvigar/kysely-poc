d381db90066c1d45fe02091308fd7a985b7978a3
Jan Vigar <jan.vigar@10xgenomics.com>
2023-08-09 16:40:13 -0700

    add k6 load testing

--- tests/api/categories.js
+++ tests/api/categories.js
@@ -1,7 +1,14 @@
 import http from "k6/http";
 import { sleep } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export default function () {
   http.get("https://kysely-poc.pages.dev/api/categories");
   sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "tests/api/categories_load_test_summary.html": htmlReport(data),
+  };
 }
